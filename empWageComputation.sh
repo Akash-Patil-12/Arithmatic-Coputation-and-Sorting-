5689f9de17592290d07ed6c12ec0065761ecda8b
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-01 10:02:15 +0530

    Take three inputs a, b & c

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,8 @@
-# Welcome in Employee Wage Computation problem
+#!/bin/bash -x
+echo "Enter a value of a"
+read a
+echo "Enter a value of b"
+read b
+echo "Enter a value of c"
+read c
+echo "Entered value are a=$a b=$b c=$c"

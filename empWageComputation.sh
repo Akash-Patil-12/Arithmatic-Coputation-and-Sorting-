db0786c0f62a70f8f24cfd9fc8dab5d8841de3c0
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-01 13:37:44 +0530

    Added : Read Dictionary value into the array

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,15 +7,22 @@ echo "Enter a value of b"
 read b
 echo "Enter a value of c"
 read c
+
 operation1=$(($(($a+$b))*$c))
 storeResult[1]=$operation1
+
 operation2=$(($(($a*$b))+$c))
 storeResult[2]=$operation2
+
 operation3=$(($(($c+$a))/$b))
 storeResult[3]=$operation3
+
 operation4=$(($(($a%$b))+$c))
 storeResult[4]=$operation4
 
-echo ${storeResult[@]}
-
-
+for ((i=0;i<4;i++))
+do
+   index=$(($i+1))
+	operationArray[(($i))]=${storeResult[$index]}
+done
+echo "operation value in array is : ${operationArray[@]}"
